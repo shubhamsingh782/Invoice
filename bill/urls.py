f35c59b94a60a3e05ec030bd07cb0ab6ca974b56
shubham <tantric.singh73@gmail.com>
2017-04-16 15:58:41 +0530

    heroku push

--- bill/urls.py
+++ bill/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
 			url(r'^Print_PDF/(?P<pk>\d+)/$', views.print_pdf, name='print_pdf'),
 			url(r'^place_order/$', views.place_order, name='place_order'),
 			url(r'^PDF/(?P<pk>\d+)/$', views.pdf_view, name='pdf_view')
-
-			]
+		]
